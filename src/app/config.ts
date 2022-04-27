353291578710a70176ec9428cfd2cd1fe249e1a8
1ike <googlohren@gmail.com>
2022-04-27 16:04:39 +0300

    Enhance title setting (set App name from env vars).

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,6 @@
-const { REACT_APP_API_BASE_URL, REACT_APP_API_USERS_RESOURCE_PATH } = process.env;
+const { REACT_APP_API_BASE_URL, REACT_APP_API_USERS_RESOURCE_PATH, REACT_APP_NAME } = process.env;
 
 
 export const API_BASE_URL = REACT_APP_API_BASE_URL || 'https://jsonplaceholder.typicode.com/';
 export const API_USERS_RESOURCE_PATH = REACT_APP_API_USERS_RESOURCE_PATH || 'users/';
+export const APP_NAME = REACT_APP_NAME || 'RooX test task';
--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -2,6 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 import type { RootState, AppDispatch } from './store';
+import { APP_NAME } from './config';
 
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
@@ -12,7 +13,7 @@ type TitleOrFnType = string | (() => string);
 export const useTitle = (titleOrFn: TitleOrFnType, deps: any[] = []) => {
   useEffect(
     () => {
-      document.title = `${typeof titleOrFn === 'function' ? titleOrFn() : titleOrFn} — RooX test task`;
+      document.title = `${typeof titleOrFn === 'function' ? titleOrFn() : titleOrFn} — ${APP_NAME}`;
     },
     deps,
   );
